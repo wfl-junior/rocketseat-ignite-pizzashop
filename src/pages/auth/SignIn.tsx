37917721f80859fbfc05607e24c9ea8a2e5bf9ae
Wallace Júnior <wflj1997@gmail.com>
2023-12-30 17:54:51 -0300

    Página: Login

--- src/pages/auth/SignIn.tsx
+++ src/pages/auth/SignIn.tsx
@@ -1,5 +1,8 @@
 import { Fragment } from "react";
 import { Helmet } from "react-helmet-async";
+import { Button } from "~/components/ui/Button";
+import { Input } from "~/components/ui/Input";
+import { Label } from "~/components/ui/Label";
 
 interface SignInProps {}
 
@@ -7,7 +10,34 @@ export function SignIn({}: SignInProps): JSX.Element | null {
   return (
     <Fragment>
       <Helmet title="Login" />
-      <h1>SignIn</h1>
+
+      <div className="p-8">
+        <div className="flex w-full max-w-[350px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Acessar painel
+            </h1>
+
+            <p className="text-sm text-muted-foreground">
+              Acompanhe suas vendas pelo painel do parceiro!
+            </p>
+          </div>
+
+          <form
+            className="space-y-4"
+            onSubmit={event => event.preventDefault()}
+          >
+            <div className="space-y-2">
+              <Label htmlFor="email">Seu e-mail</Label>
+              <Input id="email" type="email" />
+            </div>
+
+            <Button type="submit" className="w-full">
+              Acessar painel
+            </Button>
+          </form>
+        </div>
+      </div>
     </Fragment>
   );
 }
