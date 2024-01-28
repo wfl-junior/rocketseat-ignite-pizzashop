5bab4a6e101a2bc185e821b8e84805a8d92cbe6f
Wallace Júnior <wflj1997@gmail.com>
2024-01-27 22:35:06 -0300

    Cancelar pedido

--- src/pages/app/orders/OrdersTableRow.tsx
+++ src/pages/app/orders/OrdersTableRow.tsx
@@ -1,10 +1,15 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { formatDistanceToNow } from "date-fns";
 import { ptBR } from "date-fns/locale";
 import { ArrowRight, Search, X } from "lucide-react";
+import { toast } from "sonner";
+import { cancelOrder } from "~/api/cancel-order";
+import { GetOrdersResponse } from "~/api/get-orders";
 import { OrderStatus } from "~/components/OrderStatus";
 import { Button } from "~/components/ui/Button";
 import { Dialog, DialogContent, DialogTrigger } from "~/components/ui/Dialog";
 import { TableCell, TableRow } from "~/components/ui/Table";
+import { QueryKeys } from "~/lib/react-query";
 import { formatCurrency } from "~/utils/formatCurrency";
 import { OrderDetails } from "./OrderDetails";
 
@@ -21,6 +26,39 @@ interface OrdersTableRowProps {
 export function OrdersTableRow({
   order,
 }: OrdersTableRowProps): JSX.Element | null {
+  const queryClient = useQueryClient();
+  const { mutateAsync: cancelOrderFn, isPending: isCanceling } = useMutation({
+    mutationFn: cancelOrder,
+    onSuccess(_data, { orderId }) {
+      const ordersList = queryClient.getQueriesData<GetOrdersResponse>({
+        queryKey: [QueryKeys.Orders],
+      });
+
+      ordersList.forEach(([queryKey, data]) => {
+        if (!data) return;
+
+        queryClient.setQueryData<GetOrdersResponse>(queryKey, {
+          ...data,
+          orders: data.orders.map(order => {
+            if (order.orderId === orderId) {
+              return {
+                ...order,
+                status: "canceled",
+              };
+            }
+
+            return order;
+          }),
+        });
+      });
+
+      toast.success(`Pedido ${orderId} cancelado com sucesso!`);
+    },
+    onError(_error, { orderId }) {
+      toast.success(`Falha ao cancelar o pedido ${orderId}, Tente novamente.`);
+    },
+  });
+
   return (
     <TableRow>
       <TableCell>
@@ -67,7 +105,15 @@ export function OrdersTableRow({
       </TableCell>
 
       <TableCell>
-        <Button size="xs" variant="ghost" className="flex items-center gap-2">
+        <Button
+          size="xs"
+          variant="ghost"
+          className="flex items-center gap-2"
+          onClick={() => cancelOrderFn({ orderId: order.orderId })}
+          disabled={
+            isCanceling || !["pending", "processing"].includes(order.status)
+          }
+        >
           <X className="h-3 w-3" />
           Cancelar
         </Button>
